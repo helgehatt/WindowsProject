0a520c77e2d5ec656ce4ec872ea4e5a53c32b0cb
lennartpedersen <lennart-j-p@hotmail.com>
2016-10-31 11:52:32 +0100

    class diagram tool project

---
+++ ClassDiagramTool/View/UserControls/MenuUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ClassDiagramTool.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ClassDiagramTool/View/UserControls/SidePanelUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ClassDiagramTool.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SidePanelUserControl.xaml
+    /// </summary>
+    public partial class SidePanel : UserControl
+    {
+        public SidePanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
