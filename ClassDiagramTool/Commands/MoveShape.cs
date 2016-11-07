40883ff74f4f0f2113cf5fbe27342c2ed8f2139c
Thomas <dktdn@hotmail.com>
2016-11-07 21:50:24 +0100

    Experiment with dynamic EventTriggers.

--- ClassDiagramTool/Commands/MoveShape.cs
+++ ClassDiagramTool/Commands/MoveShape.cs
@@ -1,10 +1,11 @@
 ﻿using ClassDiagramTool.UndoRedo;
-using ClassDiagramTool.View.UserControls;
 using ClassDiagramTool.ViewModel.Shapes;
+using GalaSoft.MvvmLight.Command;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interactivity;
 
 namespace ClassDiagramTool.Commands
 {
@@ -23,18 +24,48 @@ namespace ClassDiagramTool.Commands
             //Setup move command.
             SetupMoveShape(MovedElement, e);
 
-            //Create Event Handlers to perform move command.
+            //Construct and add event handlers to moving element.
             MouseEventHandler MouseMove = new MouseEventHandler(
-                (object sender, MouseEventArgs e1) => { UpdateMoveShape(e1); }
-                );
+                (object sender, MouseEventArgs e1) => {
+                    UpdateMoveShape(e1);
+                });
             MouseButtonEventHandler MouseUp = null;
-            MouseUp = new MouseButtonEventHandler( //Delegate ends the move command and removes event handlers from element.
-                (object sender, MouseButtonEventArgs e2) => { FinalizeMoveShape(e2); MovedElement.MouseMove -= MouseMove; MovedElement.MouseLeftButtonUp -= MouseUp; }
-                );
+            MouseUp = new MouseButtonEventHandler( //This delegate ends the move command and removes event handlers from element.
+                (object sender, MouseButtonEventArgs e2) => {
+                    FinalizeMoveShape(e2);
+                    MovedElement.MouseMove -= MouseMove;
+                    MovedElement.MouseLeftButtonUp -= MouseUp;
+                });
 
-            //Add these to the element to be moved.
+            //Add event handlers to the element to be moved.
             MovedElement.MouseMove += MouseMove;
             MovedElement.MouseLeftButtonUp += MouseUp;
+
+            /*
+             * Following code gave inconsistent behavior because of quickly changing triggers, but shows how to add/remove a command eventtriggers to an object.
+             */
+            ////Get TriggerCollection for adding and removing EventTriggers for the moving element.
+            //System.Windows.Interactivity.TriggerCollection Triggers = Interaction.GetTriggers(MovedElement);
+
+            ////Create EventTriggers to perform move command.
+            //System.Windows.Interactivity.EventTrigger MouseMoveT = new System.Windows.Interactivity.EventTrigger("MouseMove");
+            //EventToCommand MouseMoveEventToCommand = new EventToCommand();
+            //MouseMoveEventToCommand.Command = new RelayCommand<MouseEventArgs>((MouseEventArgs e1) => { UpdateMoveShape(e1); });
+            //MouseMoveEventToCommand.PassEventArgsToCommand = true;
+            //MouseMoveT.Actions.Add(MouseMoveEventToCommand);
+
+            //System.Windows.Interactivity.EventTrigger MouseUpT = new System.Windows.Interactivity.EventTrigger("MouseLeftButtonUp");
+            //EventToCommand MouseUpEventToCommand = new EventToCommand();
+            //MouseUpEventToCommand.Command = new RelayCommand<MouseButtonEventArgs>((MouseButtonEventArgs e2) => { FinalizeMoveShape(e2);
+            //    Triggers.Remove(MouseUpT);
+            //    Triggers.Remove(MouseMoveT);
+            //});
+            //MouseUpEventToCommand.PassEventArgsToCommand = true;
+            //MouseUpT.Actions.Add(MouseUpEventToCommand);
+
+            ////Add the new triggers to the moving element.
+            //Triggers.Add(MouseUpT);
+            //Triggers.Add(MouseMoveT);
         }
 
         public bool CanExecute(object parameter)
