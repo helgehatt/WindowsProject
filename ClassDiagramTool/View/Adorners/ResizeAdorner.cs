5d1b7b0268e1706cabd6bd888bf6bcfcc189c23d
Thomas <dktdn@hotmail.com>
2016-11-21 22:54:09 +0100

    Added Resize Adorner

---
+++ ClassDiagramTool/View/Adorners/ResizeAdorner.cs
@@ -0,0 +1,182 @@
+﻿using ClassDiagramTool.ViewModel.Shapes;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ClassDiagramTool.View.Adorners
+{
+    class ResizeAdorner : Adorner
+    {
+        VisualCollection visualChildren;
+        protected override int VisualChildrenCount { get { return visualChildren.Count; } }
+        protected override Visual GetVisualChild(int index) { return visualChildren[index]; }
+
+        Thumb 
+            topLeft = new Thumb(),
+            topRight = new Thumb(),
+            bottomLeft = new Thumb(),
+            bottomRight = new Thumb(),
+            top = new Thumb(),
+            bottom = new Thumb(),
+            left = new Thumb(),
+            right = new Thumb();
+
+        public ResizeAdorner(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            visualChildren = new VisualCollection(this);
+            
+            ConstructThumb(topLeft, Cursors.SizeNWSE);
+            ConstructThumb(topRight, Cursors.SizeNESW);
+            ConstructThumb(bottomLeft, Cursors.SizeNESW);
+            ConstructThumb(bottomRight, Cursors.SizeNWSE);
+            ConstructThumb(top, Cursors.SizeNS);
+            ConstructThumb(bottom, Cursors.SizeNS);
+            ConstructThumb(left, Cursors.SizeWE);
+            ConstructThumb(right, Cursors.SizeWE);
+
+            bottomLeft.DragDelta += new DragDeltaEventHandler(HandleBottomLeft);
+            bottomRight.DragDelta += new DragDeltaEventHandler(HandleBottomRight);
+            topLeft.DragDelta += new DragDeltaEventHandler(HandleTopLeft);
+            topRight.DragDelta += new DragDeltaEventHandler(HandleTopRight);
+            top.DragDelta += new DragDeltaEventHandler(HandleTop);
+            bottom.DragDelta += new DragDeltaEventHandler(HandleBottom);
+            left.DragDelta += new DragDeltaEventHandler(HandleLeft);
+            right.DragDelta += new DragDeltaEventHandler(HandleRight);
+        }
+
+        private void HandleRight(object sender, DragDeltaEventArgs e)
+        {
+            ShapeViewModel element = (this.AdornedElement as UserControl).DataContext as ShapeViewModel;
+            Thumb hitThumb = sender as Thumb;
+
+            element.Width = Math.Max(element.Width + e.HorizontalChange, hitThumb.DesiredSize.Width);
+        }
+
+        private void HandleLeft(object sender, DragDeltaEventArgs e)
+        {
+            ShapeViewModel element = (this.AdornedElement as UserControl).DataContext as ShapeViewModel;
+            Thumb hitThumb = sender as Thumb;
+
+            double OldWidth = element.Width;
+            double NewWidth = Math.Max(element.Width + e.HorizontalChange, hitThumb.DesiredSize.Width);
+            double OldX = element.X;
+            element.X = OldX - (OldWidth - NewWidth);
+            element.Width = Math.Max(element.Width - e.HorizontalChange, hitThumb.DesiredSize.Width);
+        }
+
+        private void HandleBottom(object sender, DragDeltaEventArgs e)
+        {
+            ShapeViewModel element = (this.AdornedElement as UserControl).DataContext as ShapeViewModel;
+            Thumb hitThumb = sender as Thumb;
+
+            element.Height = Math.Max(e.VerticalChange + element.Height, hitThumb.DesiredSize.Height);
+        }
+
+        private void HandleTop(object sender, DragDeltaEventArgs e)
+        {
+            ShapeViewModel element = (this.AdornedElement as UserControl).DataContext as ShapeViewModel;
+            Thumb hitThumb = sender as Thumb;
+
+            double OldHeight = element.Height;
+            double NewHeight = Math.Max(e.VerticalChange + element.Height, hitThumb.DesiredSize.Height);
+            double OldY = element.Y;
+            element.Y = OldY - (OldHeight - NewHeight);
+            element.Height = Math.Max(element.Height - e.VerticalChange, hitThumb.DesiredSize.Height);
+        }
+
+        void HandleBottomRight(object sender, DragDeltaEventArgs e)
+        {
+            ShapeViewModel element = (this.AdornedElement as UserControl).DataContext as ShapeViewModel;
+            Thumb hitThumb = sender as Thumb;
+            
+            element.Width = Math.Max(element.Width + e.HorizontalChange, hitThumb.DesiredSize.Width);
+            element.Height = Math.Max(e.VerticalChange + element.Height, hitThumb.DesiredSize.Height);
+        }
+        
+        void HandleTopRight(object sender, DragDeltaEventArgs e)
+        {
+            ShapeViewModel element = (this.AdornedElement as UserControl).DataContext as ShapeViewModel;
+            Thumb hitThumb = sender as Thumb;
+            
+            element.Width = Math.Max(element.Width + e.HorizontalChange, hitThumb.DesiredSize.Width);
+
+            double OldHeight = element.Height;
+            double NewHeight = Math.Max(e.VerticalChange + element.Height, hitThumb.DesiredSize.Height);
+            double OldY = element.Y;
+            element.Y = OldY - (OldHeight - NewHeight);
+            element.Height = Math.Max(element.Height - e.VerticalChange, hitThumb.DesiredSize.Height);
+        }
+        
+        void HandleTopLeft(object sender, DragDeltaEventArgs e)
+        {
+            ShapeViewModel element = (this.AdornedElement as UserControl).DataContext as ShapeViewModel;
+            Thumb hitThumb = sender as Thumb;
+            
+            double OldWidth = element.Width;
+            double NewWidth = Math.Max(element.Width + e.HorizontalChange, hitThumb.DesiredSize.Width);
+            double OldX = element.X;
+            element.X = OldX - (OldWidth - NewWidth);
+            element.Width = Math.Max(element.Width - e.HorizontalChange, hitThumb.DesiredSize.Width);
+
+            double OldHeight = element.Height;
+            double NewHeight = Math.Max(e.VerticalChange + element.Height, hitThumb.DesiredSize.Height);
+            double OldY = element.Y;
+            element.Y = OldY - (OldHeight - NewHeight);
+            element.Height = Math.Max(element.Height - e.VerticalChange, hitThumb.DesiredSize.Height);
+        }
+        
+        void HandleBottomLeft(object sender, DragDeltaEventArgs e)
+        {
+            ShapeViewModel element = (this.AdornedElement as UserControl).DataContext as ShapeViewModel;
+            Thumb hitThumb = sender as Thumb;
+
+            double OldWidth = element.Width;
+            double NewWidth = Math.Max(element.Width + e.HorizontalChange, hitThumb.DesiredSize.Width);
+            double OldX = element.X;
+            element.X = OldX - (OldWidth - NewWidth);
+            element.Width = Math.Max(element.Width - e.HorizontalChange, hitThumb.DesiredSize.Width);
+
+            element.Height = Math.Max(e.VerticalChange + element.Height, hitThumb.DesiredSize.Height);
+        }
+        
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            ShapeViewModel element = (this.AdornedElement as UserControl).DataContext as ShapeViewModel;
+            double Width = element.Width;
+            double Height = element.Height;
+
+            topLeft.Arrange(new Rect(-Width / 2, -Height / 2, Width, Height));
+            topRight.Arrange(new Rect(Width - Width / 2, -Height / 2, Width, Height));
+            bottomLeft.Arrange(new Rect(-Width / 2, Height - Height / 2, Width, Height));
+            bottomRight.Arrange(new Rect(Width - Width / 2, Height - Height / 2, Width, Height));
+
+            top.Arrange(new Rect(0, -Height / 2, Width, Height));
+            bottom.Arrange(new Rect(0, Height / 2, Width, Height));
+            left.Arrange(new Rect(-Width / 2, 0, Width, Height));
+            right.Arrange(new Rect(Width / 2, 0, Width, Height));
+
+            top.Width = Width - (topLeft.Width / 2) - (topRight.Width / 2);
+            bottom.Width = Width - (bottomLeft.Width / 2) - (bottomRight.Width / 2);
+            left.Height = Height - (topLeft.Height / 2) - (bottomLeft.Height / 2);
+            right.Height = Height - (topRight.Height / 2) - (bottomRight.Height / 2);
+
+            return finalSize;
+        }
+        
+        void ConstructThumb(Thumb thumb, Cursor customizedCursor)
+        {
+            thumb.Cursor = customizedCursor;
+            thumb.Height = 10;
+            thumb.Width = 10;
+            thumb.Opacity = 1.00;
+            thumb.Background = new SolidColorBrush(Colors.AntiqueWhite);
+
+            visualChildren.Add(thumb);
+        }
+    }
+}
--- ClassDiagramTool/ViewModel/SelectedObjectsCollection.cs
+++ ClassDiagramTool/ViewModel/SelectedObjectsCollection.cs
@@ -1,8 +1,10 @@
-﻿using ClassDiagramTool.ViewModel.Shapes;
+﻿using ClassDiagramTool.View.Adorners;
+using ClassDiagramTool.ViewModel.Shapes;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace ClassDiagramTool.ViewModel
@@ -20,11 +22,17 @@ namespace ClassDiagramTool.ViewModel
         {
             (element.DataContext as ShapeViewModel).Selected = true;
             SelectionList.Add(element);
+            AdornerLayer.GetAdornerLayer(element).Add(new ResizeAdorner(element));
         }
 
         public void Select(UserControl element)
         {
-            SelectionList.ForEach((current) => { (current.DataContext as ShapeViewModel).Selected = false; });
+            SelectionList.ForEach((current) => { (current.DataContext as ShapeViewModel).Selected = false;
+                foreach (Adorner adorner in AdornerLayer.GetAdornerLayer(current).GetAdorners(current))
+                {
+                    AdornerLayer.GetAdornerLayer(current).Remove(adorner);
+                }
+            });
             SelectionList.Clear();
             AddSelect(element);
         }
@@ -33,6 +41,10 @@ namespace ClassDiagramTool.ViewModel
         {
             (element.DataContext as ShapeViewModel).Selected = false;
             SelectionList.Remove(element);
+            foreach (Adorner adorner in AdornerLayer.GetAdornerLayer(element).GetAdorners(element))
+            {
+                AdornerLayer.GetAdornerLayer(element).Remove(adorner);
+            }
         }
 
         public bool IsSelected(UserControl element) => SelectionList.Contains(element);
