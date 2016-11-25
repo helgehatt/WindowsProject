1a9f1529215e674e683e121eba1c7753514235bc
Helge Hatteland <helge.hatteland@gmail.com>
2016-11-25 12:02:13 +0100

    Modified LineControls.

--- ClassDiagramTool/ViewModel/LineViewModel.cs
+++ ClassDiagramTool/ViewModel/LineViewModel.cs
@@ -12,10 +12,7 @@ namespace ClassDiagramTool.ViewModel.Lines
     public abstract class LineViewModel : BaseViewModel, ILine
     {
         private const int OFFSET = 20;
-
-        public Line Line { get; }
-
-        public List<LinePart> LineParts { get; set; }
+        private const int ARROW = 5;
 
         private ShapeViewModel From { get; }
         private ShapeViewModel To { get; }
@@ -23,6 +20,14 @@ namespace ClassDiagramTool.ViewModel.Lines
         private ConnectionPoint FP => From.Points[FromPoint];
         private ConnectionPoint TP => To.Points[ToPoint];
 
+        public Line Line { get; }
+
+        public List<LinePart> LineParts { get; set; }
+
+        public List<Point> StartLineCap => CalculatePoints(FP);
+        public List<Point> EndLineCap   => CalculatePoints(TP);
+
+        #region Wrapper
         public int FromNumber => From.Number;
         public int ToNumber => To.Number;
 
@@ -35,12 +40,13 @@ namespace ClassDiagramTool.ViewModel.Lines
             get { return Line.Label; }
             set { Line.Label = value; }
         }
+        #endregion
 
         protected LineViewModel(Line line, ShapeViewModel from, ShapeViewModel to)
         {
             from.Lines.Add(this);
             to  .Lines.Add(this);
-
+            
             Line = line;
             From = from;
             To = to;
@@ -55,6 +61,8 @@ namespace ClassDiagramTool.ViewModel.Lines
             CalculateLinePart(FP.Orientation, FP.X, FP.Y, TP.Orientation, TP.X, TP.Y, true);
 
             OnPropertyChanged(nameof(LineParts));
+            OnPropertyChanged(nameof(StartLineCap));
+            OnPropertyChanged(nameof(EndLineCap));
         }
 
         private void CalculateLinePart(EConnectionPoint from, double X1, double Y1, EConnectionPoint to, double X2, double Y2, bool first)
@@ -107,6 +115,41 @@ namespace ClassDiagramTool.ViewModel.Lines
             LineParts.Add(new LinePart() { X1 = X1, Y1 = Y1, X2 = nextX, Y2 = nextY });
 
             CalculateLinePart(from, nextX, nextY, to, X2, Y2, false);
+        }
+
+        private List<Point> CalculatePoints(ConnectionPoint p)
+        {
+            List<Point> Points = new List<Point>();
+
+            switch(p.Orientation)
+            {
+                case EConnectionPoint.North:
+                    Points.Add(new Point(p.X - ARROW, p.Y - ARROW * 1.5));
+                    Points.Add(new Point(p.X        , p.Y              ));
+                    Points.Add(new Point(p.X + ARROW, p.Y - ARROW * 1.5));
+                    Points.Add(new Point(p.X        , p.Y - ARROW * 3.0));
+                    break;
+                case EConnectionPoint.South:
+                    Points.Add(new Point(p.X - ARROW, p.Y + ARROW * 1.5));
+                    Points.Add(new Point(p.X        , p.Y              ));
+                    Points.Add(new Point(p.X + ARROW, p.Y + ARROW * 1.5));
+                    Points.Add(new Point(p.X        , p.Y + ARROW * 3.0));
+                    break;
+                case EConnectionPoint.East:
+                    Points.Add(new Point(p.X + ARROW * 1.5, p.Y - ARROW));
+                    Points.Add(new Point(p.X              , p.Y        ));
+                    Points.Add(new Point(p.X + ARROW * 1.5, p.Y + ARROW));
+                    Points.Add(new Point(p.X + ARROW * 3.0, p.Y        ));
+                    break;
+                case EConnectionPoint.West:
+                    Points.Add(new Point(p.X - ARROW * 1.5, p.Y - ARROW));
+                    Points.Add(new Point(p.X              , p.Y        ));
+                    Points.Add(new Point(p.X - ARROW * 1.5, p.Y + ARROW));
+                    Points.Add(new Point(p.X - ARROW * 3.0, p.Y        ));
+                    break;
+            }
+
+            return Points;
         }
     }
 
